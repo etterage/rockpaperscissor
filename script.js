f99f67f8bfdedbf050c52eaed6c7d937f7a4b714
etterage <ret.napoles03@gmail.com>
2024-12-30 23:22:08 +0800

    modified the main function

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
+let win = 0;
+let lose = 0;
+let draw = 0;
+
 function playGround(playerChoice) {
     const selections = ["rock", "paper", "scissors"];
     const computerChoice = selections[Math.floor(Math.random() * selections.length)];
 
     if (playerChoice === computerChoice) {
-        alert("Player: " + playerChoice + "\nComputer: " + computerChoice)
-        alert("DRAW")
+        draw++;
+        document.getElementById('drawScore').textContent = draw;
+        document.querySelector('.resultWords p').textContent = "It's a DRAW!"
     }
     else if (
         (playerChoice === "rock" && computerChoice === "scissors") ||
         (playerChoice === "paper" && computerChoice === "rock") ||
         (playerChoice === "scissors" && computerChoice === "paper")
     ) {
-        alert("Player: " + playerChoice + "\nComputer: " + computerChoice)
-        alert("YOU WON")
+        win++;
+        document.getElementById('playerScore').textContent = win;
+        document.querySelector('.resultWords p').textContent = "You WON!"
     }
     else {
-        alert("Player: " + playerChoice + "\nComputer: " + computerChoice)
-        alert("YOU LOST")
+        lose++;
+        document.getElementById('computerScore').textContent = lose;
+        document.querySelector('.resultWords p').textContent = "You LOST!"
     }
+
+    document.querySelector('.gameContainer').style.display = 'none';
+    document.querySelector('.resultContainer').style.display = 'block';
+
+    const playerImg = document.getElementById('playerImg');
+    const computerImg = document.getElementById('computerImg');
+
+    playerImg.src = `/images/${playerChoice}.png`;
+    computerImg.src = `./images/${computerChoice}.png`
     
     
 }
@@ -30,4 +46,9 @@ document.querySelector('#paper').addEventListener('click', () => {
 });
 document.querySelector('#scissors').addEventListener('click', () => {
     playGround("scissors");
+});
+
+document.querySelector('#playAgain').addEventListener('click', () => {
+    document.querySelector('.gameContainer').style.display = 'block';
+    document.querySelector('.resultContainer').style.display = 'none';
 });
